c513a0c71ee0efd1f2cd8cd1cb0a5a573254253d
Kabina Thapa <thapakabina28@gmail.com>
2023-12-18 18:19:07 -0500

    chore:implement rendering logic to display tasks

--- index.js
+++ index.js
@@ -7,17 +7,35 @@ const tasks=[
     }
 ]
 console.log(tasks.length)
+//Render
+const render=()=>{
+    const taskContainer=document.getElementById('tasks')
+    taskContainer.innerHTML=''
+    const tasksList=tasks.map(task=>{
+        const list=document.createElement('List')
+        list.textContent=task.title
+        taskContainer.appendChild(list)
+    })
+    console.log(tasksList)
+   
+  
+
+}
+render()
 
 //Create
 const addTasks=()=>{
-    const input=document.getElementById('input').value.trim()
+   const input=document.getElementById('input')
+   const inputValue=input.value.trim()
    const newTask={
     id:tasks.length+1,
-    title:input,
+    title:inputValue,
     status:'pending'
     
 
    }
    tasks.push(newTask)
+   input.value=''
    console.log(tasks)
+   render()
 }
