f2b7a77ed4d4486194e41494bb14a658b3fe0897
Kabina Thapa <thapakabina28@gmail.com>
2023-12-20 17:31:19 -0500

    feat:add edit and update fucntionality to manipulate tasks

--- index.js
+++ index.js
@@ -1,62 +1,97 @@
-let tasks=[
+let tasks = [
     {
-        id:1,
-        title:'Do Laundry',
-        status:'completed',
-
+        id: 1,
+        title: 'Do Laundry',
+        status: 'completed',
     }
-]
-console.log(tasks.length)
-//Render
-const render=()=>{
-    const taskContainer=document.getElementById('tasks')
-    taskContainer.innerHTML=''
-    const tasksList=tasks.map(task=>{
-        const list=document.createElement('list')
-        list.className='list'
-        list.textContent=task.title
-        taskContainer.appendChild(list)
+];
 
-        const editButton=document.createElement('edit')
-        editButton.className='edit'
-        editButton.type='button'
-        editButton.textContent='edit'
-        list.appendChild(editButton)
-        
-        const deleteButton=document.createElement('delete')
-        deleteButton.className='edit'
-        deleteButton.type='button'
-        deleteButton.textContent='delete'
-        deleteButton.addEventListener('click', ()=>deleteTasks(task.id))
-        list.appendChild(deleteButton)
+console.log(tasks.length);
+const changeButton = document.getElementById('button');
+let selectedTask = null;
 
-    })
-   
-   
-  
+// Render
+const render = () => {
+    const taskContainer = document.getElementById('tasks');
+    taskContainer.innerHTML = '';
 
-}
-render()
+    tasks.forEach(task => {
+        const listItem = document.createElement('li');
+        listItem.className = 'list';
+        listItem.textContent = task.title;
+        taskContainer.appendChild(listItem);
 
-//Create
-const addTasks=()=>{
-   const input=document.getElementById('input')
-   const inputValue=input.value.trim()
-   const newTask={
-    id:tasks.length+1,
-    title:inputValue,
-    status:'pending'
-    
+        const editButton = document.createElement('button');
+        editButton.className = 'edit';
+        editButton.type = 'button';
+        editButton.textContent = 'edit';
+        editButton.addEventListener('click', () => editTasks(task));
+        listItem.appendChild(editButton);
 
-   }
-   tasks.push(newTask)
-   input.value=''
-   console.log(tasks)
-   render()
-}
-const deleteTasks=(id)=>{
-tasks=tasks.filter(task=>task.id!==id)
-render()
-console.log(tasks)
+        const deleteButton = document.createElement('button');
+        deleteButton.className = 'delete';
+        deleteButton.type = 'button';
+        deleteButton.textContent = 'delete';
+        deleteButton.addEventListener('click', () => deleteTasks(task.id));
+        listItem.appendChild(deleteButton);
+    });
+};
 
-}
+render();
+
+const input = document.getElementById('input');
+
+const handleClick = () => {
+    if (changeButton.textContent === 'Add') {
+        addTasks();
+    }
+};
+
+// Create
+const addTasks = () => {
+    const inputValue = input.value.trim();
+    if (inputValue !== '') {
+        const newTask = {
+            id: tasks.length + 1,
+            title: inputValue,
+            status: 'pending'
+        };
+        tasks.push(newTask);
+        input.value = '';
+        console.log(tasks);
+        render();
+    }
+};
+
+const deleteTasks = (id) => {
+    tasks = tasks.filter(task => task.id !== id);
+    render();
+    console.log(tasks);
+};
+
+const editTasks = (task) => {
+    selectedTask = task;
+    changeButton.textContent = 'Update';
+    changeButton.removeEventListener('click', addTasks);
+    changeButton.addEventListener('click', updateTasks);
+    input.value = task.title;
+};
+
+const updateTasks = () => {
+    const inputValue = input.value.trim();
+    if (inputValue !== '') {
+        tasks = tasks.map(task =>
+            task.id === selectedTask.id ? { ...task, title: inputValue } : task
+        );
+        console.log(tasks);
+        render();
+    }
+    input.value = ''; // Clear the input field
+    changeButton.textContent = 'Add';
+    changeButton.removeEventListener('click', updateTasks);
+
+    // Reattach the 'addTasks' event listener
+    changeButton.addEventListener('click', handleClick);
+
+    selectedTask = null; // Reset selected task
+};
